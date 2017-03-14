0f7a122ed09fde4f2287a6015760765a9a927967
Alberto He <ahe2019@room208-4.local>
2017-03-14 14:14:14 -0700

    Initial Commit

---
+++ Bullet HellTests/Bullet_HellTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bullet_HellTests.swift
+//  Bullet HellTests
+//
+//  Created by Alberto He on 3/14/17.
+//  Copyright © 2017 Alberto He. All rights reserved.
+//
+
+import XCTest
+@testable import Bullet_Hell
+
+class Bullet_HellTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
